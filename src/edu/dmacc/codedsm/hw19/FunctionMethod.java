bbdec4a3dfee23385e496f2b842a9fc4494bc33f
Stanley <skbeyan@dmacc.edu>
2019-04-24 22:57:15 -0500

    initial commit

---
+++ src/edu/dmacc/codedsm/hw19/FunctionMethod.java
@@ -0,0 +1,42 @@
+package edu.dmacc.codedsm.hw19;
+
+public class FunctionMethod {
+    public static void main(String[] args) {
+
+        String Str1 = new String("You are welcome");
+        String Str2 = new String("Study");
+
+        System.out.print("String Length:");
+        System.out.println(Str1.length());
+
+        System.out.print("String Length:");
+        System.out.println(Str2.length());
+    }
+}
+
+    public static void main(String[] args) {
+
+        int foo;
+
+        try {
+            foo = Integer.parseln("myString");
+        } catch (NumberFormatException) {
+            foo = 0;
+        }
+
+        public static void main(String[]args){
+            System.out.print("Square root of 16 is:" + Math.sqrt(16));
+        }
+
+        public static void main (String[]args){
+            String s1 = "hello";
+            String s2 = "How are you";
+            System.out.println("String length is:" + s1.length());
+            System.out.println("String length is:" + s2.length());
+        }
+    }
+}
+
+
+    }
+        }
